3487763dc8063ec353686e349293a43d8e8c3e11
Jean-Roch Masson <jyaire@gmail.com>
2020-03-14 18:25:45 +0100

    Show users who have contributed and how many pages they added

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,10 @@ namespace App\Controller;
 
 use App\Entity\Articles;
 use App\Entity\Journaux;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\NonUniqueResultException;
+use Doctrine\ORM\NoResultException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,8 +16,12 @@ class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="index")
+     * @param UserRepository $users
+     * @return Response
+     * @throws NoResultException
+     * @throws NonUniqueResultException
      */
-    public function index() :Response
+    public function index(UserRepository $users) :Response
     {
         // statistiques des journaux et des articles
         $em = $this->getDoctrine()->getManager();
@@ -33,11 +41,24 @@ class HomeController extends AbstractController
             ->getSingleScalarResult();
 
         // contributeurs
+        $contribs = $users->findAll();
+        $totArticlesContribs = [];
+        foreach ($contribs as $contrib) {
+            $totArticles = $repoArticles->createQueryBuilder('a')
+                ->select('count(a.id)')
+                ->where('a.ajouteur = :contrib')
+                ->setParameter('contrib', $contrib)
+                ->getQuery()
+                ->getSingleScalarResult();
+            array_push($totArticlesContribs, $totArticles);
+        }
 
         return $this->render('index.html.twig', [
             'totalJournaux' => $totalJournaux,
             'totalArticles' => $totalArticles,
             'totalContribs' => $totalContribs,
+            'contribs' => $contribs,
+            'totArticlesContribs' => $totArticlesContribs,
         ]);
     }
 }
