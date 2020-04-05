bbab02537bf731fabbef257d4f0079931b132d56
Jean-Roch Masson <jyaire@gmail.com>
2020-04-05 16:49:46 +0200

    Add a download button for a whole journal

--- src/Controller/JournauxController.php
+++ src/Controller/JournauxController.php
@@ -4,13 +4,14 @@ namespace App\Controller;
 
 use App\Entity\Journaux;
 use App\Form\JournauxType;
-use App\Repository\ArticlesRepository;
 use App\Repository\JournauxRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Dompdf\Dompdf;
+use Dompdf\Options;
 
 /**
  * @Route("/journaux")
@@ -59,6 +60,45 @@ class JournauxController extends AbstractController
     public function show(Journaux $journal): Response
     {
         return $this->render('journaux/show.html.twig', [
+            'journal' => $journal,
+        ]);
+    }
+
+    /**
+     * @Route("/pdf/{id}", name="journal_pdf", methods={"GET"})
+     * @param Journaux $journal
+     * @return Response
+     */
+    public function pdf(Journaux $journal): Response
+    {
+        // Configure Dompdf according to your needs
+        $pdfOptions = new Options();
+        $pdfOptions->set('defaultFont', 'Arial');
+
+        // Instantiate Dompdf with our options
+        $dompdf = new Dompdf($pdfOptions);
+
+        // Retrieve the HTML generated in our twig file
+        $html = $this->renderView('journaux/pdf.html.twig', [
+            'journal' => $journal,
+        ]);
+
+        // Load HTML to Dompdf
+        $dompdf->loadHtml($html);
+
+        // (Optional) Setup the paper size and orientation 'portrait' or 'portrait'
+        $dompdf->setPaper('A4', 'portrait');
+
+        // Render the HTML as PDF
+        $dompdf->render();
+
+        // Output the generated PDF to Browser (force download)
+        $datedebut = date_format($journal->getDatedebut(), 'Y');
+        $dompdf->stream($datedebut . ' - ' . $journal->getTitre() . '.pdf', [
+            "Attachment" => true
+        ]);
+
+        return $this->render('journaux/pdf.html.twig', [
             'journal' => $journal,
         ]);
     }
