dab3600b50b7edcffa5d8b407d2dcec601aba6e5
Jean-Roch Masson <jyaire@gmail.com>
2020-12-23 20:53:50 +0100

    Prepare password reset with maker-bundle

---
+++ src/Migrations/Version20201223195132.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20201223195132 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE reset_password_request (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, selector VARCHAR(20) NOT NULL, hashed_token VARCHAR(100) NOT NULL, requested_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', expires_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_7CE748AA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE reset_password_request ADD CONSTRAINT FK_7CE748AA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE articles CHANGE ajoutdate ajoutdate DATETIME DEFAULT NULL, CHANGE modifdate modifdate DATETIME DEFAULT NULL');
+        $this->addSql('ALTER TABLE commentaires CHANGE datecom datecom DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE likes CHANGE datelike datelike DATETIME NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE reset_password_request');
+        $this->addSql('ALTER TABLE articles CHANGE ajoutdate ajoutdate DATETIME DEFAULT NULL, CHANGE modifdate modifdate DATETIME DEFAULT NULL');
+        $this->addSql('ALTER TABLE commentaires CHANGE datecom datecom DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE likes CHANGE datelike datelike DATETIME NOT NULL');
+    }
+}
