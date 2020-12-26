fc2f87934867f416d4069897f13b07ad9876272d
Jean-Roch Masson <jyaire@gmail.com>
2020-12-26 12:14:09 +0100

    Page for new document

--- src/Form/DocumentsType.php
+++ src/Form/DocumentsType.php
@@ -6,16 +6,18 @@ use App\Entity\Documents;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 
 class DocumentsType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('file')
-            ->add('date')
-            ->add('poster')
-            ->add('article')
+            ->add('file', FileType::class, [
+                'multiple' => true,
+                'label' => null,
+                'attr' => ['class' => 'text-presentation'],
+            ])
         ;
     }
 
